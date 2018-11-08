8e3a001a42d85029a88b62f79e41b79d42ace3c9
insaneinc01 <mail.krshna@gmail.com>
2018-11-09 03:47:35 +0530

    connected to mongo atlas testdb, queries and mutation added

--- index.js
+++ index.js
@@ -1,53 +1,62 @@
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+const ObjectId = require('mongodb').ObjectId
+
+const url = 'mongodb+srv://appuser:testdb101@cluster0-ndzld.mongodb.net/test?retryWrites=true';
+const dbName = 'testdb';
+
+const client = new MongoClient(url, { useNewUrlParser: true });
+
+client.connect(async (err) => {
+  assert.equal(null, err);
+  console.log("Connected correctly to server");
+});
+
+const stringifyIds = x => {
+  x._id = x._id.toString()
+  return x
+}
+
+
 const { ApolloServer, gql } = require('apollo-server');
 
-// This is a (sample) collection of books we'll be able to query
-// the GraphQL server for.  A more complete example might fetch
-// from an existing data source like a REST API or database.
-const books = [
-  {
-    title: 'Harry Potter and the Chamber of Secrets',
-    author: 'J.K. Rowling',
-  },
-  {
-    title: 'Jurassic Park',
-    author: 'Michael Crichton',
-  },
-];
-
-// Type definitions define the "shape" of your data and specify
-// which ways the data can be fetched from the GraphQL server.
 const typeDefs = gql`
-# Comments in GraphQL are defined with the hash (#) symbol.
 
-# This "Book" type can be used in other type declarations.
-type Book {
-  title: String
-  author: String
+type Product {
+  _id: String
+  name: String
+  price: Int
+  category: String
 }
 
-# The "Query" type is the root of all GraphQL queries.
-# (A "Mutation" type will be covered later on.)
 type Query {
-  "something here"
-  books: [Book]
+  "List of all products"
+  products: [Product]
+  product(_id: String): Product
 }
+
+type Mutation {
+  createProduct(name: String, price: Int, category: String): Product
+}
+
 `;
 
-// Resolvers define the technique for fetching the types in the
-// schema.  We'll retrieve books from the "books" array above.
 const resolvers = {
   Query: {
-    books: () => books,
+    products: async () => (await client.db(dbName).collection("products").find({}).toArray()).map(stringifyIds),
+    product: async (root, {_id}) => stringifyIds(await client.db(dbName).collection("products").findOne({_id: ObjectId(_id)}))
   },
+  Mutation: {
+    createProduct: async (root, args, context, info) => {
+      const result = await client.db(dbName).collection("products").insertOne(args)
+      console.log(result.ops);
+      return stringifyIds(result.ops[0])
+    }
+  }
 };
 
-// In the most basic sense, the ApolloServer can be started
-// by passing type definitions (typeDefs) and the resolvers
-// responsible for fetching the data for those types.
 const server = new ApolloServer({ typeDefs, resolvers });
 
-// This `listen` method launches a web-server.  Existing apps
-// can utilize middleware options, which we'll discuss later.
 server.listen().then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
 });
